821b7da2cce46439052a046a65382ceb1ba422b2
Daniel Schäfer <schaefer.dev@gmail.com>
2015-08-10 19:26:28 +0200

    added forms for remaining tests

---
+++ code/np_skeleton/src/np2015/tests/TestLocalTermination.java
@@ -0,0 +1,14 @@
+package np2015.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLocalTermination {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
